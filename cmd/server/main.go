8014e0f897b5d749597a0b1c75984265a8b1dbc2
Lazarev Atakhan <lazarev_a@auca.kg>
2025-02-13 01:17:42 +0600

    godot

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,27 +4,29 @@ import (
 	"log"
 	"net/http"
 
+	// Import the os package
 	"github.com/gorilla/mux"
 	"github.com/lazarev-a-auca-2022/vpn-setup-server/internal/api"
 	"github.com/lazarev-a-auca-2022/vpn-setup-server/internal/config"
-	"github.com/lazarev-a-auca-2022/vpn-setup-server/pkg/logger" // added
+	"github.com/lazarev-a-auca-2022/vpn-setup-server/pkg/logger"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	logger.Log.Println("Server main: Loading configuration")
+
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
 	cfg, err := config.LoadConfig()
 	if err != nil {
 		log.Fatalf("Error loading config: %v", err)
 	}
-
 	router := mux.NewRouter()
-
-	certFile := "server.crt"
-	keyFile := "server.key"
-	logger.Log.Println("Server main: Starting HTTPS server on port 8443")
-	if err := http.ListenAndServeTLS(":8443", certFile, keyFile, router); err != nil {
-		log.Fatal(err)
-	}
 
 	// Public routes (no auth required)
 	router.HandleFunc("/api/auth/login", api.LoginHandler(cfg)).Methods("POST")
@@ -38,8 +40,19 @@ func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	router.PathPrefix("/").Handler(fs)
 
+	// Start HTTPS server in a goroutine
+	go func() {
+		certFile := "server.crt"
+		keyFile := "server.key"
+		logger.Log.Println("Server main: Starting HTTPS server on port 8443")
+		if err := http.ListenAndServeTLS(":8443", certFile, keyFile, router); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("HTTPS server failed to start: %v", err)
+		}
+	}()
+
+	// Start HTTP server
 	logger.Log.Printf("Server main: Server is running on port %s", cfg.Server.Port)
 	if err := http.ListenAndServe(":"+cfg.Server.Port, router); err != nil {
-		log.Fatalf("Server failed to start: %v", err)
+		log.Fatalf("HTTP server failed to start: %v", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
+	github.com/stretchr/testify v1.10.0
 	golang.org/x/crypto v0.32.0
 )
 
@@ -13,7 +15,6 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
-	github.com/stretchr/testify v1.10.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
